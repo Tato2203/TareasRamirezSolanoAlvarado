29e1c463f7406321376ce2efc5ff69eb05b8e4b8
Nador <nadiralfatec@estudiantec.cr>
2022-06-10 00:51:17 -0600

    XXXXXXXXXXXXend version XXXXXXXXXXXXXXXXXX

--- Proyecto RISCV/Verilog/Mem_Datos.v
+++ Proyecto RISCV/Verilog/Mem_Datos.v
@@ -19,41 +19,51 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-
 module Mem_Datos(
 input wire [31:0] ALUResult, //Direccion de Memoria
 input wire [31:0] WriteData, //Informacion en Memoria 
 input wire Write_EN,         //Enable de Escritura
 input wire CLK,              //Clock 
 input wire RST,
+output reg [31:0] memo, //256 Palabras de 32 bits c/u
 output reg [31:0] Read_Data //Salida Informacion en Memoria 
 );
 
-reg [31:0] MEMORIA [0:255]; //256 Palabras de 32 bits c/u
+reg [31:0] MEMORIA [0:65535];
 
-integer i;
-
+integer i,k,q;
+integer file_id;
 initial begin //Inicializando las Memorias 
 Read_Data <= 0;
-for (i=0; i<256; i=i+1)begin
-MEMORIA[i]=i;
+assign q=4294901760;
+//Se definen los espacions de memoria desde 0xffff0000
+for (i=0; i<65535; i=i+1)begin 
+MEMORIA[i]=0;
 end
+end 
+
+initial begin 
+file_id=$fopen("MEM_DUMP.txt","w"); 
 end
+
 
 always @(posedge CLK)
 if (RST) begin
 Read_Data <= 0;
 end else begin
 if (Write_EN == 1'b1)begin
-MEMORIA[ALUResult] <= WriteData;
+MEMORIA[ALUResult-q] <= WriteData;
 Read_Data <= WriteData;
-
-// agregar la escritura de ALUResult y de  WriteData a algún txt
-
 end
 else begin 
-Read_Data <= MEMORIA[ALUResult];
+Read_Data <= MEMORIA[ALUResult-q];
 end 
 end
 
+initial begin 
+#800
+for (k=0; k<70000; k=k+1) begin
+$fdisplay(file_id,"%h", k, MEMORIA[k]); 
+end
+end
 endmodule
