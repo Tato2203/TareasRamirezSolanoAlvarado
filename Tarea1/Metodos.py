f7211a652239c6e94bf277c694b3dd002da6e2ed
Tato2203 <arman2203@hotmail.com>
2022-08-02 15:47:42 -0600

    Código de principal y testing, previo al pytest

---
+++ Tarea1/Metodos.py
@@ -0,0 +1,37 @@
+
+class metodos:
+    def __init__(self):
+        self.resultado=0
+        self.cantidadCaracteres=0
+
+
+    # Método basic_operations
+    def basic_operations(self, Op1, Op2, Op):
+        tipo1 = isinstance(Op1, int) # comprobación tipo de entrada de los operandos
+        tipo2 = isinstance(Op2, int)
+        tipo3 = isinstance(Op, int)
+
+        if (tipo1 and tipo2 and tipo3):
+            if (Op==1):# Operación 1 suma
+                self.resultado = Op1 + Op2
+            elif (Op==2): # Operación 2 resta
+                self.resultado = Op1 - Op2
+            else: #Operación 3 división
+                if (Op2==0): # Comprobación división por cero
+                    return 88
+                else:
+                    self.resultado = Op1/Op2
+            return self.resultado
+
+        else:
+            return 99
+
+    # Método count_char
+    def count_char(self, param):
+        if type(param)!=str: # comprobación tipo de entrada del parámetro
+            return 77
+        else:
+            self.cantidadCaracteres = len(param) # contador de caracteres
+            return self.cantidadCaracteres
+
+
---
+++ Tarea1/Pytest.py
@@ -0,0 +1,33 @@
+import pytest
+from Metodos import metodos
+import string
+
+tarea = metodos()
+
+
+def test_1():  # revisa que la operacion basic operations realiza todas las operaciones correctamente
+    assert tarea.basic_operations(15,10,1) == 25
+    assert tarea.basic_operations(10,3,2) == 7
+    assert tarea.basic_operations(15,5,3) == 3
+
+
+def test_2():  # revisa que se retorna el codigo de error correcto si se le pasa un string como entrada
+    assert tarea.basic_operations(10,"a",1) == 99
+
+
+def test_3():  # revisa que se retorna el codigo de error correcto si se realiza una division entre 0
+    assert tarea.basic_operations(7,0,3) == 88
+
+
+def test_4(): # revisa que se cuenta correctamente la cantidad de caracteres
+    assert tarea.count_char("holamundo$$13") == 13
+
+def test_5(): # revisa que se retorne el codigo de error correcto si se pasa algo distinto a un string
+    assert tarea.count_char(80) == 77
+
+
+test_1()
+#test_2()
+#test_3()
+#test_4()
+#test_5()
